0509cd3c867dd26963238e1d69bd4d609298d6e6
firesquid <jdeiss06@gmail.com>
2023-10-08 20:15:09 -0500

    add rofi config

--- projects.py
+++ projects.py
@@ -2,17 +2,22 @@
 import rofi_menu
 import os
 
+
+EDITOR = "code"
+
+
 applications = {
     "code": "code",
     "codium": "codium",
+    "emacs": "emacs",
 }
 
 
 class Menu(rofi_menu.Menu):
     prompt = "Quick Access"
     items = [
-        rofi_menu.ShellItem("edit: config", "code /home/firesquid/.config/"),
-        rofi_menu.ShellItem("edit: personal files", "code /home/firesquid/personal/")
+        rofi_menu.ShellItem("edit: config", f"{EDITOR} /home/firesquid/.config/"),
+        rofi_menu.ShellItem("edit: personal files", f"{EDITOR} /home/firesquid/personal/")
     ]
     for application in applications:
         items.append(rofi_menu.ShellItem(
@@ -22,7 +27,7 @@ class Menu(rofi_menu.Menu):
     for filename in os.listdir(directory):
         if os.path.isdir(os.path.join(directory, filename)):
             items.append(rofi_menu.ShellItem(
-                f"project: {filename}", f"code {directory}/{filename}"))
+                f"project: {filename}", f"{EDITOR} {directory}/{filename}"))
 
 
 if __name__ == "__main__":
